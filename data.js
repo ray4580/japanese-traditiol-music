d8257dba4cb4d75af02dc3c14d06699522184f4f
ray4580 <73044163+ray4580@users.noreply.github.com>
2020-10-20 10:19:00 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+// 入力するテキストの変数
+var inputText = $(".inputText");
+
+// 出てくるテキストの変数
+var outputText = $(".outputText");
+
+// 検索ボタンをクリックした時のファンクション
+function clickSearchButton() {
+  // 入力された文字を変数に入れる
+  var PrefecturesName = inputText.val();
+
+  // シートを取得する
+  $.ajax({
+    url: "https://docs.google.com/spreadsheets/d/1jSS98HKsIkEmBeurGelht3wXTiRSpPLO8XEoKYyYD9g/edit?usp=sharing",
+    type: "GET",
+    dataType: "json",
+
+    // シートの取得に成功した時のファンクション
+    success: function (sheet) {
+      // 全てのデータを変数に入れる
+      var allData = sheet.allData;
+
+      // データの項目の数だけ検索をくりかえす
+      for (var i = 0; i < 10; i++) {
+        // もしデータの中に入力した都道府県の名前があれば
+        if (prefecturesName == allData[i][0]) {
+          // 名前の隣のデータ（曲名）を変数に入れる
+          var musicName = allData[i][1];
+        }
+      }
+      // 変数に「曲名」のテキストを表示する
+      outputText.text(musicName);
+    }
+  });
+}
